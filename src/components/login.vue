d5ec598589d470981d3ad46547271c4288794806
baiwh <471974983@qq.com>
2019-06-14 15:54:05 +0800

    基础配置完成

---
+++ src/components/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.login{
+  height: 100vh;
+  width: 100vw;
+  background: url("./../assets/login-bg.png");
+  background-size:100% 100%;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Cube from 'cube-ui'
+
+Vue.config.productionTip = false
+// 引入cube
+Vue.use(Cube)
+// 引入axios
+Vue.prototype.axios = axios
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
